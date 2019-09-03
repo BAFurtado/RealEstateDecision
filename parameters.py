d471446aee6a05ee48897e7ffd2b98507fa665b1
BAFurtado <furtadobb@gmail.com>
2019-09-03 17:02:13 -0300

    First commit

---
+++ parameters.py
@@ -0,0 +1,15 @@
+purchase_price = 832000
+downpayment = 382000
+interest_rate = .0809
+amortization_years = 20
+
+property_tax = 1
+appreciation = 3
+inflation = 2
+
+cost_renting = 2500
+return_on_cash = 6
+
+# Calculated values
+amortization_months = amortization_years * 12
+loan_amount = purchase_price - downpayment
---
+++ plotting.py
@@ -0,0 +1,25 @@
+from mortgage import Mortgage
+import parameters as p
+from itertools import islice
+
+
+if __name__ == '__main__':
+    # Order of parameters: interest_rate, months, amount
+    m = Mortgage(p.interest_rate, p.amortization_months, p.loan_amount)
+
+    # Test for month 1
+    print(m.monthly_payment())
+    print(type(m.monthly_payment()))
+
+    # Print schedule: principle and interest
+    for index, payment in enumerate(m.monthly_payment_schedule()):
+        print(payment)
+        if index == 10:
+            break
+
+    # Amount paid first 10 years
+    # Principal
+    print(sum(month[0] for month in islice(m.monthly_payment_schedule(), 240)))
+    # Interest
+    print(m.total_payout())
+
