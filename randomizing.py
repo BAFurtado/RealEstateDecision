b00f4c9fecb6fd81d571bc3dce73b6169926a1e7
BAFurtado <furtadobb@gmail.com>
2019-09-19 17:48:11 -0300

    Tickers

--- randomizing.py
+++ randomizing.py
@@ -1,13 +1,14 @@
 from collections import defaultdict
 
 import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
 import numpy as np
+from matplotlib.patches import Patch
 from pandas.plotting import register_matplotlib_converters
 
 import conf
 import generalization
 import plotting
-
 
 KEYS = ['PURCHASE_PRICE', 'DOWNPAYMENT', 'LOAN_AMOUNT',
         'INFLATION', 'TREASURE_RETURN', 'FINANCING_RATE', 'REAL_RETURN',
@@ -44,12 +45,19 @@ def plot_hist(out):
     ax = fig.add_subplot(1, 1, 1)
     rent = [o for o in out if o >= 0]
     buy = [o for o in out if o < 0]
-    ax.hist(rent, bins=50, color='green', label='Rent')
-    ax.hist(buy, bins=50, color='red', label='Buy')
+    ax.hist(rent, bins=30, color='green', alpha=.3)
+    ax.hist(buy, bins=70, color='red', alpha=.3)
     ax = plotting.basic_plot_config(ax)
+    # loc = plticker.MultipleLocator(base=1.5e6)  # this locator puts ticks at regular intervals
+    # ax.xaxis.set_major_locator(loc)
+    plt.locator_params(nbins=5)
     ax.xaxis.set_major_formatter(plt.FuncFormatter('${:,.0f}'.format))
+    legend_elements = [Patch(facecolor='green', edgecolor='green', alpha=.3, label='Rent'),
+                       Patch(facecolor='red', edgecolor='red', alpha=.3, label='Buy')]
+    ax.legend(handles=legend_elements, frameon=False, loc='best', title='Optimal decision')
+    plt.title('Histogram of comparison buy x rend with randomized parameters')
     plt.grid(True, 'major', 'y', ls='--', lw=.5, c='k', alpha=.3)
-    plt.tick_params(axis='both', which='both', bottom=False, top=False,
+    plt.tick_params(axis='both', which='both', bottom=True, top=False,
                     labelbottom=True, left=False, right=False, labelleft=True)
     plt.savefig('randomization', bbox_inches='tight')
     plt.show()
