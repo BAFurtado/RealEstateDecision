fc1d060010d63564f4d484b1ea628b0176c1f370
BAFurtado <furtadobb@gmail.com>
2019-10-03 15:34:24 -0300

    Final

--- conf/params.py
+++ conf/params.py
@@ -5,7 +5,6 @@ PURCHASE_PRICE = 400000
 DOWNPAYMENT = 120000
 LOAN_AMOUNT = PURCHASE_PRICE - DOWNPAYMENT
 HOUSE_APPRECIATION = .05
-
 FINANCING_RATE = .07
 
 # Alternatives: 'price', 'sac'
@@ -14,24 +13,24 @@ SELLING_COST = .06
 CONTRACT_DATE = datetime.date(2019, 9, 17)
 
 # Borrowers' age (mutuários)
-BIRTH1 = datetime.date(1980, 3, 28)
-BIRTH2 = datetime.date(1977, 9, 1)
+BIRTH1 = datetime.date(1969, 9, 23)
+BIRTH2 = datetime.date(1970, 9, 1)
 
 PERC_BORROWER1 = 1
 PERC_BORROWER2 = 1 - PERC_BORROWER1
 
 # Macroeconomics
 INFLATION = .0375
-TREASURE_RETURN = .0512
+TREASURE_RETURN = .0537
+
+# Rental -- about 3.5% of home value per year
+RENT_PERCENTAGE = .0029
+RENT_RAISING_PERIOD = 12
 
 # Adjustment fees
 # If correction includes inflation, add below
 REFERENCIAL_FEE = 0
 # REFERENCIAL_FEE = INFLATION
-
-# Rental -- about 3.5% of home value per year
-RENT_PERCENTAGE = .0029
-RENT_RAISING_PERIOD = 12
 
 AMORTIZATION_MONTHS = 240
 
@@ -43,5 +42,5 @@ REAL_RETURN = round((TREASURE_RETURN - INFLATION) * (1 - TAX), 6)
 REAL_HOUSE_APPRECIATION = round((HOUSE_APPRECIATION - INFLATION), 6)
 
 # To save, give data a path and a name
-DATA = None
-# DATA = 'data.csv'
+# DATA = None
+DATA = 'data.csv'
